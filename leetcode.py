b54ea9ac5c3eca94af3ad24061bf1f15edf48bac
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-09-14 00:52:12 +0800

    269. Alien Dictionary

--- leetcode.py
+++ leetcode.py
@@ -1967,6 +1967,61 @@ class Solution:
         print(init)
         return init
 
+    # hint
+    # 這題很難，可以多用幾個例子來理解
+    # 第一個部分是建立圖形，第二個是DFS利用dict去歷遍所有點
+    # 要了解visited的True和False的意義
+    # 要了解dfs所return的True和False的意義
+    def alienOrder(self, words: list) -> str:
+        adj = {char: set() for word in words for char in word}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            minLen = min(len(w1), len(w2))
+            # ex: ["waf", "wa"] -> 這順序有問題,直接回空
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            # 如何把圖弄出來
+            # 2個字開始比，若第一個就不一樣，做dict起來
+            # ex: ["wrf", "er"] -> {w:set(e)}
+            # ex: ["wrt", "wrf"] -> {t:set(f)}
+            # 直到所有都做完
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    # print(w1[j], w2[j])
+                    adj[w1[j]].add(w2[j])
+                    break
+
+        visited = {}  # {char: bool} False visited, True current path
+        res = []
+
+        def dfs(char):
+            # base case
+            if char in visited:
+                return visited[char]
+
+            visited[char] = True
+
+            # 使用dict和dfs往下找
+            for neighChar in adj[char]:
+                # 若是回true，則表示碰到cycle
+                if dfs(neighChar):
+                    return True
+
+            visited[char] = False
+            res.append(char)
+            return False
+
+        # 每隔char都找過一次
+        for char in adj:
+            # 若為True表示有cycle
+            if dfs(char):
+                return ""
+
+        # 因為使用
+        res.reverse()
+        print(res)
+        return "".join(res)
 
 if __name__ == '__main__':
     soultion = Solution()
@@ -2118,4 +2173,6 @@ if __name__ == '__main__':
     # soultion.canAttendMeetings(intervals = [(0,30)])
     # soultion.minMeetingRooms(intervals = [(0,30), (5,10), (15,20)])
     # soultion.validTree(n=5, edges=[[0,1],[0,2],[0,3],[0,4]])
-    soultion.missingNumber(nums = [3,0,1])
+    # soultion.missingNumber(nums = [3,0,1])
+    soultion.alienOrder(words=["wrt", "wrf", "er", "ett", "rftt"])
+    # soultion.alienOrder(words=["we", "ee", "we"])
