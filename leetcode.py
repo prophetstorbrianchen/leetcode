a80ff0fc926a163b900b50e656d05966f7934f5f
brian.chen <brian.chen@prophetstor.com>
2022-09-17 15:32:55 +0800

    323. Number of Connected Components in an Undirected Graph

--- leetcode.py
+++ leetcode.py
@@ -2237,6 +2237,43 @@ class Solution:
             print(dp[amount])
             return dp[amount]
 
+    # hint
+    # Union Find的演算法
+    # 這題還是不懂，需要多看幾次，這題的觀念很重要
+    # 多看筆記和影片
+    # https://www.youtube.com/watch?v=8f1XPm4WOUc
+    def countComponents(self, n: int, edges: list) -> int:
+        par = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            res= n1
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return 0
+
+            if rank[p2] > rank[p1]:
+                par[p1] = p2
+                rank[p2] = rank[p2] + rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] = rank[p1] + rank[p2]
+
+            return 1
+
+        res = n
+        for n1, n2 in edges:
+            res = res - union(n1, n2)
+
+        return res
+
 
 if __name__ == '__main__':
     soultion = Solution()
@@ -2414,4 +2451,5 @@ if __name__ == '__main__':
     """
 
     # soultion.lengthOfLIS(nums = [10,9,2,5,3,7,101,18])
-    soultion.coinChange(coins = [1,2,5], amount = 11)
+    # soultion.coinChange(coins = [1,2,5], amount = 11)
+    soultion.countComponents(n=5, edges = [[0,1],[1,2],[3,4]])
