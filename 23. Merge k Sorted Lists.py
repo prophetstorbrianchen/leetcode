6bc29397a79ee8948690e1d8d19eecc89e832725
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-10-10 23:48:03 +0800

    20221010

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -98,6 +98,40 @@ class Solution:
             lists = mergedLists
         return lists[0]
 
+    def mergeKLists_2(self, lists: [[ListNode]]) -> [ListNode]:
+        def mergeList(l1, l2):
+            dummy = ListNode()
+            tail = dummy
+
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2 = l2.next
+                tail = tail.next
+
+            if l1:
+                tail.next = l1
+            else:
+                tail.next = l2
+            return dummy.next
+
+        # **注意edge case**
+        if not lists or len(lists) == 0:
+            return None
+
+        # **這個是22merge的技巧一定要會，有點類似BFS的模板 -> O(nlogn)**
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(mergeList(list1, list2))
+            lists = mergedLists
+        return lists[0]
+
 
 if __name__ == '__main__':
     solution = Solution()
@@ -106,4 +140,5 @@ if __name__ == '__main__':
     head2 = buildLinkList.build([1, 3, 4])
     head3 = buildLinkList.build([2, 6])
 
-    solution.mergeKLists(lists=[head1, head2, head3])
+    #solution.mergeKLists(lists=[head1, head2, head3])
+    solution.mergeKLists_2(lists=[head1, head2, head3])
