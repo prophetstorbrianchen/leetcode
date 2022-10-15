e0266a0cd8938e00b304b95b5b37c3d8aec70da7
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-10-16 01:17:41 +0800

    20221015

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -20,7 +20,31 @@ class Solution:
             print(dp[amount])
             return dp[amount]
 
+    def coinChange_2(self, coins: [int], amount: int) -> int:
+        # define dp
+        dp = [float("inf")] * (amount + 1)
+
+        # base case
+        dp[0] = 0
+
+        # algo
+        for n in range(1, amount + 1):
+            for c in coins:
+                # ** n等於或大於c才要減**
+                # [1,3,4,7], amount=7
+                # 當dp[2]時，也就是2塊的硬幣能怎麼換，只能換1塊，沒辦法換3/4/7塊
+                # 怎麼換可以看筆記
+                if n >= c:
+                    dp[n] = min(dp[n], dp[n - c] + 1)
+
+        if dp[amount] == [float("inf")]:
+            print(dp[amount])
+            return -1
+        else:
+            print(dp[amount])
+            return dp[amount]
+
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.coinChange(coins = [3,5], amount = 4)
+    solution.coinChange_2(coins = [1,2,5], amount = 11)
