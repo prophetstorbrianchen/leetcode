f0141732e39c9620555020eeb58a8b1a9cdd69d0
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-10-28 23:56:37 +0800

    Improve leetcode 735

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -7,8 +7,8 @@ class Solution:
                 stack.append(item)
                 continue
 
-            temp = 100
-            while temp != 0:
+            notCollession = False
+            while notCollession is False:
                 if not stack:
                     stack.append(item)
                     break
@@ -16,19 +16,20 @@ class Solution:
                 if (item > 0 and stack[-1] > 0) or (item < 0 and stack[-1] < 0) or (stack[-1] < 0 and item > 0):
                     # 同向/負在左正在右
                     stack.append(item)
-                    temp = 0
+                    notCollession = True
                 else:
                     # 相向
                     # 開始比，看誰大，放入stack
                     if abs(stack[-1]) > abs(item):
-                        temp = 0
+                        notCollession = True
                     elif abs(stack[-1]) < abs(item):
-                        temp = stack.pop()
+                        stack.pop()
+                        notCollession = False
                     elif abs(stack[-1]) == abs(item):
                         if stack[-1] == item:
                             # [-2 ] -2
                             stack.append(item)
-                            temp = 0
+                            notCollession = True
                         else:
                             # [2] -2
                             stack.pop()
@@ -39,4 +40,4 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.asteroidCollision(asteroids = [8,-8])
+    solution.asteroidCollision(asteroids = [-2,-1,1,-2])
