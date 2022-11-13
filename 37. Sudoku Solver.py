613cf06e61c305671d4520074abb33a9a1559325
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-11-14 01:15:41 +0800

    20221113

--- 37. Sudoku Solver.py
+++ 37. Sudoku Solver.py
@@ -4,6 +4,9 @@ import collections
 class Solution:
     # hint
     # 不知道怎麼設backtracking
+    # 不太懂回傳true/false的這種dfs或是backtracking
+    # https://leetcode.cn/problems/sudoku-solver/solution/37-jie-shu-du-hui-su-sou-suo-suan-fa-xiang-jie-by-/
+    # https://leetcode.cn/problems/sudoku-solver/solution/pythonsethui-su-chao-guo-95-by-mai-mai-mai-mai-zi/
     # https://leetcode.com/problems/sudoku-solver/discuss/866333/Python%3A-Backtracking-%2B-HashMapsSets-%2B-Complexity-%2B-Explanation -> 這個方法向N皇后那堤，可以try看看
     # https://blog.csdn.net/L141210113/article/details/88419719
     def __init__(self):
@@ -11,6 +14,7 @@ class Solution:
         self.col_table = collections.defaultdict(set)
         # key為tuple -> (r/3, c/3); dict的key不能為list,但可以為tuple
         self.square_table = collections.defaultdict(set)
+        self.empty = []
 
     def isValidSudoku(self, board: [[str]]) -> bool:
         rows = len(board)
@@ -20,6 +24,7 @@ class Solution:
             for c in range(cols):
                 # 把結果記錄在這3個table裡面
                 if board[r][c] == ".":
+                    self.empty.append((r, c))
                     continue
                 else:
                     if board[r][c] not in self.row_table[r] and board[r][c] not in self.col_table[c] and board[r][c] not in self.square_table[(int(r / 3), int(c / 3))]:
@@ -38,39 +43,86 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        """
-        res = []
+        # method 1 -> 只用正向的方式，紀錄所有未走過的位置，以及不可用的數字
         self.isValidSudoku(board)
-        def dfs(i, board):
-            if i == 8:
-                return
-            
-            for r in range(9):
-                for c in range(9):
-                    if board[r][c] == '.':
-                        for val in '123456789':
-                            if val in self.row_table[r] or val in self.col_table[c] or val in self.square_table[(int(r / 3), int(c / 3))]:
-                                continue
 
-                            # place:
-                            board[r][c] = val
-                            self.row_table[r].add(val)
-                            self.col_table[c].add(val)
-                            self.square_table[(int(r / 3), int(c / 3))].add(val)
+        def dfs(iter):
+            # 把空的都補完了
+            # base case
+            if iter == len(self.empty):  # 处理完empty代表找到了答案
+                return True
 
-                            # next:
-                            dfs(i + 1, board)
+            # 解析出空的空格位置
+            r, c = self.empty[iter]
 
-                            # remove:
-                            board[r][c] = "."
-                            self.row_table[r].remove(val)
-                            self.col_table[c].remove(val)
-                            self.square_table[(int(r / 3), int(c / 3))].remove(val)
+            for val in '123456789':
+                if val in self.row_table[r] or val in self.col_table[c] or val in self.square_table[(int(r / 3), int(c / 3))]:
+                    continue
 
-        dfs(0, board)
+                # place:
+                board[r][c] = val
+                self.row_table[r].add(val)
+                self.col_table[c].add(val)
+                self.square_table[(int(r / 3), int(c / 3))].add(val)
+
+                # next:
+                # 如果為True，就返回
+                # 如果為False，就返回上一層，並remove掉之前寫得直
+                # 使用bool的dfs就是這種用法 -> 藥習慣
+                if dfs(iter + 1):
+                    return True
+
+                # remove:
+                # 退回上層
+                board[r][c] = "."
+                self.row_table[r].remove(val)
+                self.col_table[c].remove(val)
+                self.square_table[(int(r / 3), int(c / 3))].remove(val)
+            return False
+
+        dfs(0)
         print(board)
+
+
+        """
+        # method 2 -> 只用反向的方式，紀錄所有未走過的位置，以及可用的數字
+        row = [set(range(1, 10)) for _ in range(9)]  # 行剩余可用数字
+        col = [set(range(1, 10)) for _ in range(9)]  # 列剩余可用数字
+        block = [set(range(1, 10)) for _ in range(9)]  # 块剩余可用数字
+
+        empty = []  # 收集需填数位置
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':  # 更新可用数字
+                    val = int(board[i][j])
+                    row[i].remove(val)
+                    col[j].remove(val)
+                    block[(i // 3) * 3 + j // 3].remove(val)
+                else:
+                    empty.append((i, j))
+
+        def backtrack(iter=0):
+            if iter == len(empty):  # 处理完empty代表找到了答案
+                return True
+            i, j = empty[iter]
+            b = (i // 3) * 3 + j // 3
+            for val in row[i] & col[j] & block[b]:
+                row[i].remove(val)
+                col[j].remove(val)
+                block[b].remove(val)
+                board[i][j] = str(val)
+                if backtrack(iter + 1):
+                    return True
+                row[i].add(val)  # 回溯
+                col[j].add(val)
+                block[b].add(val)
+            return False
+
+        backtrack()
         """
 
+        """
+        # method 3 -> 容易理解，但不知道True和False的意思
         def isValid(x, y):
             tmp = board[x][y];
             board[x][y] = 'D'
@@ -99,6 +151,7 @@ class Solution:
 
         dfs(board)
         print(board)
+        """
 
 
 
