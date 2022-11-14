864809c8fb53e38aadce90c43cd682dc855202b1
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-11-15 00:59:38 +0800

    20221114

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -42,7 +42,34 @@ class Solution:
             return len(nums) - count
         """
 
+    def removeElement_2(self, nums: [int], val: int) -> int:
+        # 先做sort -> 這種sort，是內部的swap，並非assign一個新值
+        nums.sort()
+
+        # val沒有在nums的情況
+        if val not in nums:
+            return len(nums)
+
+        # 紀錄val的index，之後要換位置
+        val_index_list = []
+        val_count = 0
+        for i, n in enumerate(nums):
+            if n == val:
+                val_count = val_count + 1
+                val_index_list.append(i)
+
+        # 位置交換 -> 2要被後面的3和4assign就好，也不需要交換(因為只檢查前面)
+        # [0,0,1,2,2,2,3,4] -> [0,0,1,3,4,2,3,4]
+        for i in range(len(nums)):
+            # 這是推出來的公式，可以自己畫一次
+            if i >= (val_index_list[-1] + 1):
+                nums[i - val_count] = nums[i]
+
+        print(nums)
+        return len(nums) - val_count
+
+
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.removeElement(nums = [0,1,2,2,3,0,4,2], val = 2)
+    solution.removeElement_2(nums = [0,1,2,2,3,0,4,2], val = 2)
