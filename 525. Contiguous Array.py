14d615df7c66769c823126ead59292270075d39f
brian.chen <brian.chen@prophetstor.com>
2022-11-16 18:14:54 +0800

    20221116

--- 525. Contiguous Array.py
+++ 525. Contiguous Array.py
@@ -33,10 +33,36 @@ class Solution:
         print(max_length)
         return max_length
 
+    def findMaxLength_2(self, nums: [int]) -> int:
+        # 使用prefix sum和hash table
+        # 0 -> -1; 1 -> +1
+        # 小技巧，必須多使用一個0在前面; {num: index}
+        # nums:            [0 ,1 ,0]
+        # prefix_sum: [0, -1, 0, -1]
+        prefix_hash_table = {0: 0}
+
+        init_prefix_sum = 0
+        max_length = 0
+        for i, n in enumerate(nums):
+            if n == 0:
+                init_prefix_sum = init_prefix_sum - 1
+            else:
+                init_prefix_sum = init_prefix_sum + 1
+
+            if init_prefix_sum not in prefix_hash_table:
+                prefix_hash_table[init_prefix_sum] = i + 1
+            else:
+                origin_index = prefix_hash_table[init_prefix_sum]
+                new_index = i + 1
+                max_length = max(max_length, new_index - origin_index)
+
+        print(max_length)
+        return max_length
+
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.findMaxLength(nums = [1,0,1])
+    solution.findMaxLength_2(nums = [1,0,1,0,0,1])
 
 
 
