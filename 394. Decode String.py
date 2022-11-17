9649acb7b3b2437979ef9ebb8e498fec04cdd7dc
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-11-18 01:26:58 +0800

    20221117

--- 394. Decode String.py
+++ 394. Decode String.py
@@ -33,7 +33,49 @@ class Solution:
         print(res)
         return res
 
+    def decodeString_2(self, s: str) -> str:
+        # 從正序去loop，有碰到右括號時的先處理，然後放入stack -> "3[a]2[bc]" -> 先處理3a存入stack -> [aaa, bcbc]
+        # 碰到巢狀時，就會自動從後面往前處理 -> "3[a2[c]]" -> 3[acc] -> [accaccacc]
+        # 使用倒敘的方式loop回來
+        # 碰到右括號直接pop
+        # 結算到左括號前的string -> 看左括號前面數字是多少就loop幾次
+        # 算完再放回去
+
+        stack = []
+        # 重點在於用從頭開始loop，我自己寫的時候，只有解到巢狀結構是正確的而已
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                # 到著來
+                string = ""
+                while stack[-1] != "[":
+                    tmp_c = stack.pop()
+                    # **這樣就reverser了**
+                    string = tmp_c + string
+
+                # 碰到左括號 -> 把左括號pop掉
+                stack.pop()
+
+                # **碰到數字 -> 處理數字, 這真的很神要學**
+                k = ""
+                while stack and stack[-1].isdigit():
+                    # **這樣就正序了**
+                    k = stack.pop() + k
+
+                # 乘回去，放入stack
+                tmp_string = ""
+                for _ in range(int(k)):
+                    tmp_string = tmp_string + string
+                stack.append(tmp_string)
+                # stack.append(int(k) * string)
+
+        print(str(stack))
+        res = "".join(stack)
+        print(res)
+        return res
+
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.decodeString(s = "2[abc]0[cd]ef")
+    solution.decodeString_2(s = "100[leetcode]")
