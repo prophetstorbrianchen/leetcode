1c4a897c0d7e556c94e086273737336ecc8aeb27
prophetstorbrianchen <brian.chen@prophetstor.com>
2022-11-21 00:55:45 +0800

    20221120

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution:
     # hint
     # 走過就要轉為0，之後才不會重複算
@@ -87,11 +90,78 @@ class Solution:
         print(count)
         return count
 
+    def numIslands_3(self, grid: [[str]]) -> int:
+        # BFS寫法
+        # 殭屍擴散的方法，使用q.appendleft
+        # 分別給為1的island，不同的號碼
+        # 因為一直擴散出去，所以相互連通的會是同一組號碼，不相互連通的會有不同的號碼
+        # 最後只要看有幾個不同的號碼就可以了
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        # 給定號碼
+        q = collections.deque()
+        seen = set()
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "0":
+                    seen.add((r, c))
+                    continue
+                else:
+                    q.append((r, c, count))
+                count = count + 1
+
+        island_count = set()
+
+        # BFS
+        while q:
+            r, c, index = q.popleft()
+            # 被擴散出去的絕對會在seen中，而獨立的必定不再seen中
+            # 用此可以在island_count中得到不同的號碼 -> 有幾種不同的號碼就表示有幾個不連通的島嶼
+            if (r, c) not in seen:
+                seen.add((r, c))
+                island_count.add(index)
+
+            # 上
+            if 0 <= r - 1 < rows and 0 <= c < cols and (r - 1, c) not in seen:
+                island_count.add(index)
+                # 如果使用append就會跑到後面，那index就不會被蓋掉了 -> appendleft是殭屍擴散法的精隨
+                q.appendleft((r - 1, c, index))
+                seen.add((r - 1, c))
+
+            # 下
+            if 0 <= r + 1 < rows and 0 <= c < cols and (r + 1, c) not in seen:
+                island_count.add(index)
+                q.appendleft((r + 1, c, index))
+                seen.add((r + 1, c))
+
+            # 左
+            if 0 <= r < rows and 0 <= c - 1 < cols and (r, c - 1) not in seen:
+                island_count.add(index)
+                q.appendleft((r, c - 1, index))
+                seen.add((r, c - 1))
+
+            # 右
+            if 0 <= r < rows and 0 <= c + 1 < cols and (r, c + 1) not in seen:
+                island_count.add(index)
+                q.appendleft((r, c + 1, index))
+                seen.add((r, c + 1))
+
+        print(len(island_count))
+        return len(island_count)
+
+    def numIslands_4(self, grid: [[str]]) -> int:
+        # BFS寫法
+        # 看是使用了幾次BFS，count就要加+1
+        # 跟DFS方法是一樣的
+        pass
 
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.numIslands(grid = [
+    solution.numIslands_3(grid = [
   ["1","1","0","0","0"],
   ["1","1","0","0","0"],
   ["0","0","1","0","0"],
