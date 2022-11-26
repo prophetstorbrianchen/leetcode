e3e146334a48d901aac1c72cf771bc8f4fcf409d
brian.chen <brian.chen@prophetstor.com>
2022-11-26 18:15:02 +0800

    20221126

--- interview.py
+++ interview.py
@@ -9,8 +9,27 @@ class Solution:
 
         return newString
 
+class Rectangle(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
 
 if __name__ == '__main__':
+
+
+
     solution = Solution()
     # test case
     print(solution.stringReplace(s="Today is Monday, we are interviewing", target="Monday", replacedValue="Tuesday"))
@@ -23,3 +42,8 @@ if __name__ == '__main__':
     print(solution.stringReplace(s="Today is Monday, we are interviewing", target="Monday", replacedValue="Monday"))
 
     print(solution.stringReplace(s="Today is Monday, we are interviewing", target="Mon", replacedValue="Tuesday"))
+
+    # 考繼承
+    square = Square(10)
+    print(square.area())
+    print(square.perimeter())
