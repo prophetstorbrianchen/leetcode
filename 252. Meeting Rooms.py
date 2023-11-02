55cd8152eb842db3fd52d04d0c656383728aeded
brian.chen <brianchen231@gmail.com>
2023-11-02 23:37:17 +0800

    20231102

--- 252. Meeting Rooms.py
+++ 252. Meeting Rooms.py
@@ -19,7 +19,27 @@ class Solution:
         print(True)
         return True
 
+    def canAttendMeetings_1(self, intervals: list) -> bool:
+        sorted_intervals = sorted(intervals)
+        start = sorted_intervals[0][0]
+        end = sorted_intervals[0][1]
+
+        if len(intervals) == 1:
+            print(True)
+            return True
+
+        for i in range(1, len(sorted_intervals)):
+            if sorted_intervals[i][0] > start and sorted_intervals[i][1] >= end:
+                start = sorted_intervals[i][0]
+                end = sorted_intervals[i][1]
+            else:
+                print(False)
+                return False
+        print(True)
+        return True
+
 
 if __name__ == '__main__':
     solution = Solution()
     solution.canAttendMeetings(intervals=[(0, 30), (15, 20), (5, 10)])
+    solution.canAttendMeetings_1(intervals=[(0, 30), (15, 20), (5, 10)])
